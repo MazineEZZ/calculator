99ce9a15a9b213057c5840bf877cbc5d1d5ca506
MazineEZZ <mazinewolf2007@gmail.com>
2026-02-16 21:31:46 +0000

    Core logic is now working and stable. Need to fix some functional bugs before I move to the UI

--- script.js
+++ script.js
@@ -1,40 +1,74 @@
-let inputBox = document.querySelector("#input-box");
-let value1 = "";
-let value2 = "0";
-let toggle = false;
+const inputBox = document.querySelector("#input-box");
+let param = {
+    curr: "",
+    total: "",
+    lastOper: "",
+    isNewCalc: false
+}
 
+inputBox.addEventListener("keydown", (event) => {
+    // Stops certain characters from being entered
+    const allowedKeys = ['Backspace', 'ArrowLeft', 'ArrowRight', 'Tab'];
+    if (!/[0-9]/.test(event.key) && !allowedKeys.includes(event.key)) {
+        event.preventDefault(); 
+    }
 
-inputBox.addEventListener("keydown", (e) => {
-    if (e.key >= "0" && e.key <= "9") {
-        changeValues(e.key);
-    } else if (e.key === "+") {
-        calcPrev()
-        toggle = true;
+    if (/[0-9]/.test(event.key) && param.isNewCalc) {
+        param.curr = "";
+        param.total = "";
+        param.lastOper = "";
+        param.isNewCalc = false;
         inputBox.value = "";
-    } else if (e.key === "Enter") {
-        getResult();
+    }
+
+    if (event.key >= "0" && event.key <= "9") {
+        event.preventDefault();
+        changeValues(event.key);
+        inputBox.value = param.curr;
+    } else if (["+", "-", "*", "/"].includes(event.key)) {
+        if (param.total === "") {
+            param.total = param.curr;
+            param.curr = "0";
+        } else {
+            getCalc(event.key);
+        }
+        param.lastOper = event.key;
+        inputBox.value = "";
+        event.preventDefault();
+        param.isNewCalc = false;
+    } else if (event.key === "Enter") {
+        inputBox.value = Math.floor(getCalc(param.lastOper) * 1000) / 1000;
+        event.preventDefault();
+        param.isNewCalc = true;
     }
 })
 
 function changeValues(digit) {
-    if (!toggle) {
-        value1 += digit;
-    } else {
-        value2 += digit;
+    param.curr += digit;
+}
+
+function getCalc(oper) {
+    if ((param.curr === "" || parseInt(param.curr) === 0) && oper === "/") {
+        return "ERROR";
+    } else if (param.curr === "" && (oper === "*" || oper === "/"))  {
+        param.curr = "1";
+    } else if (param.curr === "") {
+        param.curr = "0";
     }
-}
 
-function getResult() {
-    let result = parseInt(value1) + parseInt(value2);
-    inputBox.value = result;
+    const a = Number(param.total);
+    const b = Number(param.curr);
     
-    value1 = result;
-    value2 = "";
-    toggle = false;
+    switch (oper) {
+        case "+": param.total = a + b; break;
+        case "-": param.total = a - b; break;
+        case "*": param.total = a * b; break;
+        case "/": param.total = a / b; break;
+    }
+
+    param.curr = "";
+    return param.total;
 }
 
-function calcPrev() {
-    value1 = parseInt(value1) + parseInt(value2);
-    console.log(value1);
-    value2 = "";
-}
+// TODO: * Backspace is non-functional only visual
+//       * The "Chain" Execution Problem (Check with Gemini for more information)
